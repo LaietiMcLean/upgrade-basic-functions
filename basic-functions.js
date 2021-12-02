0647ad13eaa0986e28c9791b0adf411f7a4f9d42
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2021-12-02 13:29:39 +0100

    Iteration#2

--- basic-functions.js
+++ basic-functions.js
@@ -5,3 +5,33 @@ function sum(numberOne , numberTwo) {
 
     //Testeo de la función:
         sum(10,6); //Devuelve 10
+
+//Iteración 2
+const avengers = [
+    'Hulk', 
+    'Thor', 
+    'IronMan', 
+    'Captain A.', 
+    'Spiderman', 
+    'Captain M.',
+    'Una palabra muy larga'
+];
+
+
+    function findLongestWord(param) {
+        let longest = param[0];
+        param.forEach((long) => {
+            let longitudMasLargo = longest.length;
+            let longitudValorActual = long.length;
+            if (longitudValorActual > longitudMasLargo) {
+                longest = long;
+            } 
+        })
+        return longest;
+        };
+
+        let resultado = findLongestWord(avengers);
+        console.log(resultado);
+ 
+
+    
